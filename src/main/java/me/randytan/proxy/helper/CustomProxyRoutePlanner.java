737187e3faf37c933cb56d725d2c5560e67dc674
randytan <randy@randytan.me>
2018-10-08 22:41:42 +0800

    Adding logic to lookup properties file if no arguments supplied.

--- src/main/java/me/randytan/proxy/helper/CustomProxyRoutePlanner.java
+++ src/main/java/me/randytan/proxy/helper/CustomProxyRoutePlanner.java
@@ -1,5 +1,6 @@
 package me.randytan.proxy.helper;
 
+import me.randytan.proxy.model.SystemProp;
 import org.apache.http.HttpException;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
@@ -11,7 +12,7 @@ import org.apache.logging.log4j.Logger;
 
 public class CustomProxyRoutePlanner extends DefaultRoutePlanner  {
 
-    static final Logger LOG = LogManager.getLogger(RequestHandler.class);
+    static final Logger LOG = LogManager.getLogger(CustomProxyRoutePlanner.class);
 
     public CustomProxyRoutePlanner(SchemePortResolver schemePortResolver) {
         super(schemePortResolver);
@@ -19,10 +20,28 @@ public class CustomProxyRoutePlanner extends DefaultRoutePlanner  {
 
     private HttpHost getProxy(String scheme) {
 
-        String proto = scheme;
+        String protocol = scheme;
 
-        String proxyHostKey = proto + ".proxyHost";
-        String proxyPortKey = proto + ".proxyPort";
+        String proxyHostKey = protocol + ".proxyHost";
+        String proxyPortKey = protocol + ".proxyPort";
+
+        if(proxyHostKey == ""){
+            if ((SystemProp.getProxyAddress() != null) || (SystemProp.getProxyAddress() != "")) {
+                proxyHostKey = SystemProp.getProxyAddress();
+            } else {
+                LOG.warn("Please provide Proxy Host URL by adding parameter or inside properties file.");
+            }
+        }
+
+        if(proxyPortKey == ""){
+            if ((SystemProp.getProxyPort() != null)||(SystemProp.getProxyPort() != "")){
+                proxyPortKey = SystemProp.getProxyPort();
+            } else {
+                LOG.warn("Please provide Proxy Port by adding parameter or inside properties file.");
+            }
+        }
+
+
         String protoProxyHost = System.getProperty(proxyHostKey);
         if (protoProxyHost == null) {
             protoProxyHost = System.getenv(proxyHostKey);
@@ -49,7 +68,7 @@ public class CustomProxyRoutePlanner extends DefaultRoutePlanner  {
         if (protoProxyPort < 1) {
             return null;
         }
-        LOG.debug("set {} proxy '{}:{}'", proto, protoProxyHost, protoProxyPort);
+        LOG.debug("set {} proxy '{}:{}'", protocol, protoProxyHost, protoProxyPort);
         return new HttpHost(protoProxyHost, protoProxyPort, "http");
     }
 
